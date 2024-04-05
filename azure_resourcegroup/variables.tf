ec27269c03d5ccd45c7262b2e9ea304b5a2c7112
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-05 11:14:43 +0530

    Update variables.tf

--- azure_resourcegroup/variables.tf
+++ azure_resourcegroup/variables.tf
@@ -4,18 +4,4 @@ variable "rgname" {
 variable "location" {
   type = string
 }
-variable "application" {
-  type = string
-}
-variable "owner" {
-  type = string
-}
-variable "department" {
-  type = string
-}
-variable "env" {
-  type = string
-}
-variable "costcenter" {
-  type = string
-}
+
