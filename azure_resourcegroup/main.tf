26c0cf7fd001f6c326cd9d4d799226a1e975b955
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-05 14:22:08 +0530

    Update main.tf

--- azure_resourcegroup/main.tf
+++ azure_resourcegroup/main.tf
@@ -1,10 +1,3 @@
-provider "azurerm" {
-  subscription_id = var.TF_VAR_ARM_SUBSCRIPTION_ID
-  client_id       = var.TF_VAR_ARM_CLIENT_ID
-  client_secret   = var.TF_VAR_ARM_CLIENT_SECRET
-  tenant_id       = var.TF_VAR_ARM_TENANT_ID
-}
-
 resource "azurerm_resource_group" "this" {
   name     = var.rgname
   location = var.location
