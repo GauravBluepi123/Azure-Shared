9636ece362fb3ea2646a4cded432af6d72ea0fc8
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-07 17:18:34 +0530

    Update main.tf

--- azure_resourcegroup/main.tf
+++ azure_resourcegroup/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.0"
+    }
+  }
+}
 resource "azurerm_resource_group" "this" {
   name     = var.rgname
   location = var.location
