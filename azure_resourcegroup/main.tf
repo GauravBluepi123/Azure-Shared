2961af63b15ec2802540989087f746f4f5c45a89
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-07 17:13:57 +0530

    Update main.tf

--- azure_resourcegroup/main.tf
+++ azure_resourcegroup/main.tf
@@ -1,13 +1,12 @@
-resource "azurerm_resource_group" "this" {
-  name     = var.rgname
-  location = var.location
-}
 provider "azurerm" {
-  features {}
+  features {}  
   subscription_id = "f400423d-4718-4f96-9e32-4b3989422b16"
   client_id       = "d5a511b9-e410-4ba3-91f1-c454ae29aea1"
   client_secret   = "iwg8Q~5SZe~hpj.BQRIPNRKZpxb6UdvC-qwSCc~r"
   tenant_id       = "41ce6cb6-896d-4960-9d5b-54466d916c1c"
 }
 
-
+resource "azurerm_resource_group" "this" {
+  name     = var.rgname
+  location = var.location
+}
