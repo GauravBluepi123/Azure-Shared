e0e0c2340ec5f1bd176f3f75900b58291b5a7408
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-09 17:46:39 +0530

    a

--- azure_vm/vnet.tf
+++ azure_vm/vnet.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "this" {
 
 resource "azurerm_subnet" "this" {
   name                 = var.subnet_name
-  virtual_network_name = var.virtual_network_name
+  virtual_network_name = azurerm_virtual_network.this.name
   resource_group_name  = var.rgname
   address_prefixes     = ["10.0.1.0/24"]
 }
