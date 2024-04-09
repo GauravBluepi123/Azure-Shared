a11b8f35e85d05effd80c84a3faf21bf6a02291c
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-09 16:16:20 +0530

    a

--- azure_vm/variables.tf
+++ azure_vm/variables.tf
@@ -7,13 +7,7 @@ variable "location" {
 variable "virtual_network_name" {
   type = string
 }
-variable "ip-addrs_virtual_network" {
-  type = string
-}
 variable "subnet_name" {
-  type = string
-}
-variable "subnet_ip_prefix" {
   type = string
 }
 variable "public_ip_name" {
--- azure_vm/vnet.tf
+++ azure_vm/vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "this" {
   name                = var.virtual_network_name
-  address_space       = var.ip-addrs_virtual_network
+  address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.rgname
 }
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "this" {
   name                 = var.subnet_name
   virtual_network_name = var.virtual_network_name
   resource_group_name  = var.rgname
-  address_prefixes     = var.subnet_ip_prefix
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "this" {
